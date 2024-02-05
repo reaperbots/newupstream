719ff5331a1181fbfd236f552d503d46ecdf7d6b
reaperbots <148609775+reaperbots@users.noreply.github.com>
2024-02-05 16:34:19 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -157,7 +157,7 @@ def source(self):
 
 
 def get_readable_message():
-    msg = '<b>⚡Powered By: <a href="https://telegram.me/Reaperzclub">Reaperzclub</a></b>\n\n'
+    msg = ''
     button = None
     tasks = len(download_dict)
     currentTime = get_readable_time(time() - botStartTime)
