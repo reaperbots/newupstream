2fc70882647a14650b379ea08605e8e78abaa7cd
reaperbots <148609775+reaperbots@users.noreply.github.com>
2024-03-06 21:29:48 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -171,7 +171,7 @@ def get_readable_message():
         globals()['PAGE_NO'] = PAGES
     for download in list(download_dict.values())[STATUS_START:STATUS_LIMIT+STATUS_START]:
         msg += f"<u><b>⌬ {escape(f'{download.name()}')}</u></b>\n\n"
-        msg += f"<q><b>┌{download.status()}...</b>"
+        msg += f"<blockquote><q><b>┌{download.status()}...</b>"
         if download.status() not in [MirrorStatus.STATUS_SPLITTING, MirrorStatus.STATUS_SEEDING]:
             msg += f"\n<b>├</b><code>{progress_bar(download.progress())}</code> <b>{download.progress()}</b>"
             msg += f"\n<b>├Processed: </b>{download.processed_bytes()} of {download.size()}"
@@ -192,7 +192,7 @@ def get_readable_message():
         else:
             msg += f"\n<b>├Size: {download.size()}</b>"
         msg += f"\n<b>├Past:</b> {get_readable_time(time() - download.message.date.timestamp())}"
-        msg += f"\n<b>└/stop_{download.gid()[:8]}</b></q>\n\n"
+        msg += f"\n<b>└/stop_{download.gid()[:8]}</b></blockquote></q>\n\n"
     if len(msg) == 0:
         return None, None
     dl_speed = 0
