057f57f4aed4745f18f0896ef0e792b69a105dcf
reaperbots <148609775+reaperbots@users.noreply.github.com>
2024-03-25 10:06:47 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -209,7 +209,7 @@ def get_readable_message():
         buttons.ibutton(f"{PAGE_NO}/{PAGES}", "status ref")
         buttons.ibutton("Next", "status nex")
         button = buttons.build_menu(3)
-    msg += f"<b>Up: {currentTime} | Free: {get_readable_file_size(disk_usage('/usr/src/app/downloads/').free)}</b>"
+    msg += f"\n<b>Up: {currentTime} »» Free: {get_readable_file_size(disk_usage('/usr/src/app/downloads/').free)}</b>"
     return msg, button
 
 
